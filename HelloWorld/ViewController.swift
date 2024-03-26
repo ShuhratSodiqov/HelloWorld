d8c15d8fec394455b15e48f0ccd15a86513f48e1
ShuhratSodiqov <sadykov_shukhrat_01@mail.ru>
2024-03-26 16:19:08 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Shuhrat Sodiqov on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
